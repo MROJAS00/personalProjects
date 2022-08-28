ce74ee069ba55249c02c60cf13bc3162262cbba4
Miguel Rojas <miguelrojas@admins-MacBook-Air.local>
2022-08-28 17:43:12 +0200

    adding gcp sample

---
+++ 01-sample-infra-gcp/provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+project     = "shining-axon-231409"
+region      = "eu-west1"
+}
+
+
---
+++ 01-sample-infra-gcp/variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  default = "shining-axon-231409"
+}
+
+variable "controller_count" {
+  default = 1
+}
+
+variable "region" {
+    default = "europe-west1"
+
+}
